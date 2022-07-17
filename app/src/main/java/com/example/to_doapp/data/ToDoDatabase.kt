d850ddc2700547a4fc234f4a5dc342ad7f590d2b
Mzazi25 <langat.caleb25@gmail.com>
2022-07-17 18:04:19 +0300

    added database class which extends from the room database

--- app/src/main/java/com/example/to_doapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/to_doapp/data/ToDoDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.to_doapp.data
 
-class ToDoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.to_doapp.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase(){
+
+    abstract fun toDoDao(): ToDoDao
 }

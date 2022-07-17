c832c35f93f47bd4f211b16f8b0711dfc9164ea2
Mzazi25 <langat.caleb25@gmail.com>
2022-07-17 17:29:41 +0300

    Added deleteTask, deleteAllTasks and searchQuery functions to our DAO

--- app/src/main/java/com/example/to_doapp/data/ToDoDao.kt
+++ app/src/main/java/com/example/to_doapp/data/ToDoDao.kt
@@ -1,8 +1,6 @@
 package com.example.to_doapp.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import com.example.to_doapp.data.models.ToDoTask
 import kotlinx.coroutines.flow.Flow
 
@@ -14,6 +12,18 @@ interface ToDoDao {
     @Query("SELECT * FROM todo_table WHERE id=:taskId")
     fun getSelectedTask(taskId: Int): Flow<ToDoTask>
 
-    @Insert
-    fun addTask(toDoTask: ToDoTask)
+    @Insert(onConflict =OnConflictStrategy.IGNORE)
+    suspend fun addTask(toDoTask: ToDoTask)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDataBase(searchQuery: String): Flow<List<ToDoTask>>
 }

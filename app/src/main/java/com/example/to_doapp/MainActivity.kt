d99dc7d6e94f9474a2b804faf103a4499d0cbccc
Mzazi25 <langat.caleb25@gmail.com>
2022-07-17 15:43:43 +0300

    Added dependencies for Room, DataStore preference and Dagger hilt on gradle files

--- app/src/main/java/com/example/to_doapp/MainActivity.kt
+++ app/src/main/java/com/example/to_doapp/MainActivity.kt
@@ -16,28 +16,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToDoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            ToDoAppTheme{
+
+            }
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoAppTheme {
-        Greeting("Android")
-    }
-}

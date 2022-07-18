15cf423ef7d64ec0276a98d6028cc12966034378
Mzazi25 <langat.caleb25@gmail.com>
2022-07-18 23:23:44 +0300

    Added getAllTask to the sharedview models which is a coroutine scope

--- app/src/main/java/com/example/to_doapp/ui/theme/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/example/to_doapp/ui/theme/viewModels/SharedViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.to_doapp.ui.theme.viewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.to_doapp.data.models.ToDoTask
 import com.example.to_doapp.data.repository.ToDoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(private val repository: ToDoRepository):ViewModel() {
 
+   private val _allTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
+    val allTask:StateFlow<List<ToDoTask>> = _allTasks
+    fun getAllTask(){
+        viewModelScope.launch {
+            repository.getAllTask.collect {
+                _allTasks.value = it
+            }
+        }
+    }
 }

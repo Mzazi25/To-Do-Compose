95d11668767a8ca46a631f2252b95a931352418a
Mzazi25 <langat.caleb25@gmail.com>
2022-07-18 23:12:27 +0300

    Injected shared view model with ToDoRepository

--- app/src/main/java/com/example/to_doapp/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/to_doapp/data/repository/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.example.to_doapp.data.repository
 
 import com.example.to_doapp.data.ToDoDao
 import com.example.to_doapp.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
     val getAllTask: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
     val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
--- app/src/main/java/com/example/to_doapp/ui/theme/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/example/to_doapp/ui/theme/viewModels/SharedViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.to_doapp.ui.theme.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.example.to_doapp.data.repository.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SharedViewModel:ViewModel() {
+@HiltViewModel
+class SharedViewModel @Inject constructor(private val repository: ToDoRepository):ViewModel() {
+
 }

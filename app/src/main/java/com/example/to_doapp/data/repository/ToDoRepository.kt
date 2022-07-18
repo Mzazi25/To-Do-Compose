0e62a7f4d5a3dba723ba308f691c4609140b13b2
Mzazi25 <langat.caleb25@gmail.com>
2022-07-18 22:33:14 +0300

    Added variables on the ToDoRepository to access functions on our DAO

--- app/src/main/java/com/example/to_doapp/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/to_doapp/data/repository/ToDoRepository.kt
@@ -1,4 +1,13 @@
 package com.example.to_doapp.data.repository
 
-class ToDoRepository {
+import com.example.to_doapp.data.ToDoDao
+import com.example.to_doapp.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+    val getAllTask: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
 }

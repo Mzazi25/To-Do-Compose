39e2ef9c3c5d97fe9e69efb1bef323f57f9d56db
Mzazi25 <langat.caleb25@gmail.com>
2022-07-19 18:02:53 +0300

    Created NavHost Controller

--- app/src/main/java/com/example/to_doapp/navigation/SetupNavigation.kt
+++ app/src/main/java/com/example/to_doapp/navigation/SetupNavigation.kt
@@ -1,9 +1,21 @@
 package com.example.to_doapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.to_doapp.util.Constants.LIST_SCREEN
 
 @Composable
 fun SetupNavigation(navController: NavHostController) {
+    val screen = remember(navController){
+        Screens(navController)
+    }
+    NavHost(
+        navController = navController,
+        startDestination = LIST_SCREEN
+    ){
 
+    }
 }

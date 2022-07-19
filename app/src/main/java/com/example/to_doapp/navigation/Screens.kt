6cc67f1cf8bdbdf6ace3c068c61e198df3cde1ad
Mzazi25 <langat.caleb25@gmail.com>
2022-07-19 17:55:06 +0300

    Added task variable which will navigate to the tasksId

--- app/src/main/java/com/example/to_doapp/navigation/Screens.kt
+++ app/src/main/java/com/example/to_doapp/navigation/Screens.kt
@@ -1,7 +1,17 @@
 package com.example.to_doapp.navigation
 
 import androidx.navigation.NavHostController
+import com.example.to_doapp.util.Action
+import com.example.to_doapp.util.Constants.LIST_SCREEN
+import com.example.to_doapp.util.Constants.TASK_SCREEN
 
 class Screens(navController:NavHostController) {
-
+    val list: (Action)-> Unit = { action ->
+        navController.navigate("List/${action.name}") {
+            popUpTo(LIST_SCREEN)
+        }
+    }
+    val task:(Int) -> Unit = { taskId ->
+        navController.navigate("Task/${taskId}")
+    }
 }

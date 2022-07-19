5b9ee6bbd3210e303cf8aa631ae38ebfe3d42d47
Mzazi25 <langat.caleb25@gmail.com>
2022-07-19 17:57:10 +0300

    Setup new file SetupNavigation

---
+++ app/src/main/java/com/example/to_doapp/navigation/SetupNavigation.kt
@@ -0,0 +1,9 @@
+package com.example.to_doapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SetupNavigation(navController: NavHostController) {
+
+}

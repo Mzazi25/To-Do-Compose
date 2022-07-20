b9dc3964883c4259ed60afa40c6c83c511014607
Mzazi25 <langat.caleb25@gmail.com>
2022-07-20 08:40:02 +0300

    Added Scaffold for the List Screen

--- app/src/main/java/com/example/to_doapp/ui/theme/screen/ListScreen.kt
+++ app/src/main/java/com/example/to_doapp/ui/theme/screen/ListScreen.kt
@@ -1,8 +1,38 @@
 package com.example.to_doapp.ui.theme.screen
 
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListScreen(navigateToTaskScreen : (Int) -> Unit) {
 
+    Scaffold(
+        content = {},
+        floatingActionButton = {
+            ListFab()
+        }
+    )
+}
+
+@Composable
+fun ListFab() {
+    FloatingActionButton(
+        onClick = { }
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add Task" )
+    }
+}
+
+@Preview
+@Composable
+fun ListScreenPreview() {
+    ListScreen(navigateToTaskScreen = {})
 }

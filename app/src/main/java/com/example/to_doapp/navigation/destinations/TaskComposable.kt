0a2225886a96fe76e9c990d2109dbdc132933bf1
Mzazi25 <langat.caleb25@gmail.com>
2022-07-21 22:00:34 +0300

    Added Task Composable up the hierarchy to the setup Nav

--- app/src/main/java/com/example/to_doapp/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/to_doapp/navigation/destinations/TaskComposable.kt
@@ -4,18 +4,22 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.to_doapp.ui.theme.screen.tasks.TaskScreen
 import com.example.to_doapp.util.Action
 import com.example.to_doapp.util.Constants
+import com.example.to_doapp.util.Constants.TASK_ARGUMENTS
 
 fun NavGraphBuilder.taskComposable(
     navigateToListScreen : (Action) -> Unit
 ){
     composable(
         route = Constants.TASK_SCREEN,
-        arguments = listOf(navArgument(Constants.TASK_ARGUMENTS){
+        arguments = listOf(navArgument(TASK_ARGUMENTS){
             type = NavType.IntType
         })
-    ){
+    ){ navBackStackEntry ->
+        val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENTS)
 
+        TaskScreen(navigateToListScreen = navigateToListScreen)
     }
 }
--- app/src/main/java/com/example/to_doapp/ui/theme/screen/tasks/TaskScreen.kt
+++ app/src/main/java/com/example/to_doapp/ui/theme/screen/tasks/TaskScreen.kt
@@ -3,12 +3,15 @@ package com.example.to_doapp.ui.theme.screen.tasks
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import com.example.to_doapp.util.Action
 
 @Composable
-fun TaskScreen() {
+fun TaskScreen(
+    navigateToListScreen: (Action) -> Unit
+) {
     Scaffold(
         topBar = {
-
+                TaskAppBar(navigateToListScreen =navigateToListScreen)
         },
         content = {
 

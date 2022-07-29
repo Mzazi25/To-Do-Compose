7aaf0a42bcd60b30b34f6836efb9b71f25aeddce
Mzazi25 <langat.caleb25@gmail.com>
2022-07-29 12:33:52 +0300

    Added animation to the splash screen logo

--- app/src/main/java/com/example/to_doapp/ui/theme/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/to_doapp/ui/theme/screen/splash/SplashScreen.kt
@@ -1,15 +1,21 @@
 package com.example.to_doapp.ui.theme.screen.splash
 
 import android.window.SplashScreen
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,19 +27,42 @@ import com.example.to_doapp.ui.theme.ToDoAppTheme
 import com.example.to_doapp.ui.theme.splashScreen
 import com.example.to_doapp.util.Constants.SPLASH_SCREEN_DELAY
 import kotlinx.coroutines.delay
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
+
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+
     LaunchedEffect(key1 = true ){
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.splashScreen),
+            .background(MaterialTheme.colors.splashScreen)
+            .offset(y = offsetState)
+            .alpha(alpha = alphaState)
+        ,
         contentAlignment = Alignment.Center
     ) {
         Image(
